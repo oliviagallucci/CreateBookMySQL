77d9bc12a7956e58597089bea4a8abf6773d9217
Olivia A. Gallucci <63474548+oliviagallucci@users.noreply.github.com>
2022-03-13 18:08:23 -0400

    Modificaitons
    Use these commands to verify if modifications work properly on the database.

---
+++ swen261_database_mod_check.sql
@@ -0,0 +1,14 @@
+select * from author;
+select * from book;
+select * from bookauthor;
+select * from bookcondition;
+select * from bookreview;
+select * from category;
+select * from inventory;
+select * from permrole;
+select * from publisher;
+select * from purchaselog;
+select * from reviewer;
+select * from state;
+select * from user;
+
